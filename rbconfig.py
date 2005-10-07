3f952dab036fd5711e25b9f3e4a9b50e11868de3
cns <empty>
2005-10-07 14:42:42 +0000

    We can set quotas again!

--- rbconfig.py
+++ rbconfig.py
@@ -13,7 +13,7 @@ import random
 # DATA                                                                #
 #---------------------------------------------------------------------#
 
-__version__ = '$Revision: 1.7 $'
+__version__ = '$Revision: 1.8 $'
 __author__  = 'Cillian Sharkey'
 
 # Find out where the rrs directory is.
@@ -225,11 +225,9 @@ def gen_quotas(usertype = None):
 	
 	"""
 
-	# XXX: /webtree moved to deathray, on NFS now. Fix this when useradm
-	# is moved to deathray.
-
 	return {
-		#'/webtree': (125000, 135000, 20000, 30000)
+		'/webtree': (175000, 200000, 800000, 1000000),
+		'/home': (175000, 200000, 800000, 1000000)
 	}
 
 def gen_extra_user_files(username):
